24dbf5e4da4d48f252dc367faacbd442f2ff06da
elenoa429 <flea0jzs161@gmail.com>
2017-10-12 16:03:57 +0900

    エフェクトマネージャーを追加
    エフェクトマネージャーの使い方は
    EffectContoroller.csを参照。
    
    テスト用でPrefab,Materialにあるので必要なくなった場合は削除してください。

---
+++ Assets/Scripts/EffectController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectController : MonoBehaviour
+{
+
+    //使用するエフェクトを追加
+    public enum Effects
+    {
+        TEST_EFFECT0 = 0,
+        TEST_EFFECT1 = 1,
+        TEST_EFFECT2 = 2
+    }
+
+    //エフェクト用の配列
+    [SerializeField]
+    private ParticleSystem[] effectArray;
+
+    // エフェクトの生成
+    public ParticleSystem EffectCreate(Effects type, Transform transform)
+    {
+        int num = (int)type; //引数で渡された番号をキャストして渡す
+        ParticleSystem obj;
+        obj = Instantiate(effectArray[num], transform.position, transform.rotation); //effect生成
+        obj.transform.parent = transform; //生成時のエンティティと親子する
+        return obj;
+    }
+}
