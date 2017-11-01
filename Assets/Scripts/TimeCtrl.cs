7818c014533294db6eabba950d5f74215c87dec5
elenoa429 <flea0jzs161@gmail.com>
2017-11-01 18:29:56 +0900

    Playerの挙動変更及びスコア、タイムのUIの変更
    Playerはwheelcolliderの追加、調整
    ガクガクするバグをRigidbodyのRotationFreezeで解決
    ※wheelcolliderは要調整
    
    タイムは位置の調整及びスクリプト内で正常な動作をするように変更しました。
    タイムの間に「:」を入れ差し替えするだけでそのまま使えるようにしました。
    追加：colonPrefab
    
    スコアも同様でタイムと同様の処理を行いました。
    追加：無し

--- Assets/Scripts/TimeCtrl.cs
+++ Assets/Scripts/TimeCtrl.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class TimeCtrl : MonoBehaviour
 {
 
-    const int TIME_MAX = 6;    //スコアの桁数
+    const int TIME_MAX = 4;    //スコアの桁数
     const float TIME_DEFAULT_POS = -1.5f; //スコアの1の位の基本になる位置(いい感じの値) 
     const int TOTAL_TIME = 500;
 
@@ -13,50 +13,78 @@ public class TimeCtrl : MonoBehaviour
 
     private int TimeMinute;     //分のカウント
     private int TimeSeconds;    //秒のカウント
-    private int TimeConma;      //秒以下の数字
     private int TotalTime;      //制限時間
 
     private GameObject[] TimeArray;    //スコアの桁数
     private float Timeleft;
 
+    private GameObject colonObj;
+
     public GameObject TimePrefab;
     public Sprite[] SpriteArray;    //スコアのテクスチャ
 
-    public float white_space;        //数字1つ1つの間隔を決める用
+    public GameObject colonPrefab;  //数字間の　「:」これ
 
-    public GameObject[] ConmaArray;    //コンマ専用配列
-    private int[] ConmaStack;           //コンマ専用スタック
+    [SerializeField]
+    private float padding;  //1文字の間隔
+
+    [SerializeField]
+    private float heightPadding;    //縦の間隔
+
+    [SerializeField]
+    private float colonPadding;     //コロンの間隔
 
     [SerializeField]
     Sprite numberSp;
+
+    [SerializeField]
+    Sprite colonSp;
+
+    //private int TimeConma;      //秒以下の数字
+    //public GameObject[] ConmaArray;    //コンマ専用配列
+    //private int[] ConmaStack;           //コンマ専用スタック
 
     void Start()
     {
         //分,秒の初期化
         TimeMinute = 5;
         TimeSeconds = 60;
-        TimeConma = 0;
         TotalTime = TOTAL_TIME;
 
         //スコアの実際に表示される0~9の値を格納する変数
         TimeStack = new int[ TIME_MAX ];
         TimeArray = new GameObject[ TIME_MAX ];
 
+        float colonSize = (float)colonSp.texture.width / 2.0f;
         //桁数分タイムを生成する
-        float unitSize = ( float )numberSp.texture.width / 10.0f;
+        float unitSize = ( float )numberSp.texture.width / 10.0f;   //1文字分の幅を取る
 
         for( int nCnt = 0 ; nCnt < TIME_MAX ; nCnt++ )
         {
-            //pos = new Vector3( TimePrefab.transform.position.x + TIME_DEFAULT_POS - ( white_space * nCnt ) , 6.0f , 0.0f );
+            TimeArray[nCnt] = Instantiate(TimePrefab);
             Vector3 pos;
-            pos.x = -( ( float )Screen.width / 2.0f ) / 2.0f + unitSize * nCnt;
-            pos.y = ( ( float )Screen.height / 2.0f ) - 20.0f;
+            pos.x = ((float)Screen.width / 2.0f) / 1.5f + unitSize * nCnt + (padding * nCnt); // 1920/2 /2
+            pos.y = ((float)Screen.height / 2.0f) - 20.0f - heightPadding;
             pos.z = 0.0f;
 
-            TimeArray[ nCnt ] = Instantiate( TimePrefab , pos , Quaternion.identity ); //Time生成
-
-            TimeArray[ nCnt ].transform.parent = gameObject.transform;   //生成されたTimeArrayに元のTimeに親子関係を紐づけする
+            TimeArray[nCnt].transform.position = pos; //Time生成
+            TimeArray[nCnt].transform.parent = gameObject.transform;   //生成されたTimeArrayに元のTimeに親子関係を紐づけする
         }
+        for (int nCnt = 2; nCnt < 4; nCnt ++)
+        {
+            Vector3 PaddingPos;
+            PaddingPos = new Vector3(colonSize, 0.0f, 0.0f);
+            TimeArray[nCnt].transform.position += PaddingPos;
+            TimeArray[nCnt].transform.parent = gameObject.transform;
+        }
+        colonObj = Instantiate(colonPrefab);
+        Vector3 colonPos;
+        colonPos.x = TimeArray[1].transform.position.x + colonSize * 1.5f;
+        colonPos.y = TimeArray[1].transform.position.y;
+        colonPos.z = 0.0f;
+
+        colonObj.transform.position = colonPos;
+        colonObj.transform.parent = gameObject.transform;
     }
 
     void Update()
@@ -67,9 +95,6 @@ public class TimeCtrl : MonoBehaviour
             Timeleft = 0.0f;
             TimeSet( 1 );
         }
-
-        TimeConma++;
-        ConmaSet( TimeConma );
     }
 
     /************************************************************
@@ -93,27 +118,24 @@ public class TimeCtrl : MonoBehaviour
         }
 
         //入ってきたスコアをチェック
-        //TimeStack[7] = totalTime / 10000000 % 10; //10000000の位
-        //TimeStack[6] = totalTime / 1000000 % 10;  //1000000の位
+        TimeStack[ 0 ] = TimeMinute / 10 % 10;     //10の位
+        TimeStack[ 1 ] = TimeMinute % 10;      //1の位
 
-        TimeStack[ 5 ] = TimeMinute / 10 % 10;   //10の位
-        TimeStack[ 4 ] = TimeMinute % 10;    //1の0
-
-        TimeStack[ 3 ] = TimeSeconds / 10 % 10;     //10の位
-        TimeStack[ 2 ] = TimeSeconds % 10;      //1の位
-
-        //TimeStack[1] = TimeConma % 10;       //10の位
-        //TimeStack[0] = TimeConma % 10;            //1の位
+        TimeStack[ 2 ] = TimeSeconds / 10 % 10;       //10の位
+        TimeStack[ 3 ] = TimeSeconds % 10;            //1の位
 
         //各TimeArrayにTimeStackに合ったスプライトに差し替える
         for( int nCnt = 0 ; nCnt < TIME_MAX ; nCnt++ )
         {
             TimeArray[ nCnt ].GetComponent<SpriteRenderer>().sprite = SpriteArray[ TimeStack[ nCnt ] ];
         }
-        //TimeZeroCheck();   //Timeのゼロチェック（仮）
     }
 
-    private void ConmaSet( int Conma )
+    //------------------------もう使わなくなったやつ------------------------///
+    //秒以下のコンマの表示と時間のゼロチェック
+
+    //秒以下の設定
+    /*private void ConmaSet( int Conma )
     {
         int TotalConma = Conma;
 
@@ -122,7 +144,7 @@ public class TimeCtrl : MonoBehaviour
 
         TimeArray[ 0 ].GetComponent<SpriteRenderer>().sprite = SpriteArray[ TimeStack[ 0 ] ];
         TimeArray[ 1 ].GetComponent<SpriteRenderer>().sprite = SpriteArray[ TimeStack[ 1 ] ];
-    }
+    }*/
 
     /************************************************************
     * 関数名：TimeZeroCheck
@@ -130,6 +152,7 @@ public class TimeCtrl : MonoBehaviour
     * 戻り値:なし
     * 説明：スコアの使われている桁を表示させて使われていない桁を非表示にする
     ***********************************************************/
+    /*
     private void TimeZeroCheck()
     {
         int TimeValueCnt = TIME_MAX;          //桁数をカウントする用
@@ -150,5 +173,5 @@ public class TimeCtrl : MonoBehaviour
         {
             TimeArray[ nCnt ].SetActive( true );
         }
-    }
+    }*/
 }
