8144b29d8c2f3b656105d570b212cf41da618d77
SyunichiSato <Kurage-dog@outlook.jp>
2017-11-07 15:52:29 +0900

    結合用
    結合用

--- Assets/Scripts/MapIcon.cs
+++ Assets/Scripts/MapIcon.cs
@@ -4,11 +4,24 @@ using UnityEngine;
 
 public class MapIcon : MonoBehaviour {
 
+    public enum TYPE
+    {
+        TYPE_PLAYER = 0,
+        TYPE_GUEST,
+        TYPE_FAR_GUEST
+    }
+    public TYPE type;
+
     GameObject human;   // 親オブジェクトの乗客
+    GameObject mapCamera;   // マップカメラ
+
 	// Use this for initialization
 	void Start () {
-        //親オブジェクトを取得
+        // 親オブジェクトを取得
         human = transform.root.gameObject;
+
+        // マップカメラ
+        mapCamera = GameObject.Find("MapCamera");
     }
 	
 	// Update is called once per frame
@@ -18,6 +31,12 @@ public class MapIcon : MonoBehaviour {
         if(state != 2 && state != 1 && state != 0)
         {// もし表示する必要のない状態なら消す。
             Destroy(gameObject);
+        }
+
+        if(type == TYPE.TYPE_FAR_GUEST)
+        {
+            Vector3 mapCameraPos = mapCamera.transform.position;
+            transform.position = transform.position = new Vector3();
         }
 	}
 }
