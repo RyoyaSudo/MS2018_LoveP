2f1073becb501a92f0381728d23c5a944d2fe695
elenoa429 <flea0jzs161@gmail.com>
2017-11-16 12:48:11 +0900

    障害物にスクリプトを追加

---
+++ Assets/MoveObstacle.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObstacle : MonoBehaviour {
+
+    private Rigidbody obstacleRb;
+
+    [SerializeField]
+    private float addPower; //加える力
+
+    [SerializeField]
+    private float impactRate;　//Velocity（向き）の力
+
+    private Vector3 velocity;
+
+    private Vector3 pos;
+	// Use this for initialization
+	void Start () {
+        obstacleRb = GetComponent<Rigidbody>();
+        pos = GetComponent<Transform>().position;
+        velocity = obstacleRb.velocity;
+	}
+	
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Player")
+        {
+            Debug.Log("ぶつかったぜ");
+            velocity.y += velocity.y * addPower;
+            //pos = new Vector3(pos.x + addPower, pos.y + addPower, pos.z + addPower);
+            obstacleRb.AddForce(transform.forward * addPower, ForceMode.Impulse);
+            obstacleRb.AddForce(velocity * impactRate, ForceMode.Impulse);
+        }
+    }
+}
