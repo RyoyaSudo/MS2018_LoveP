e86961f641fcdae88100d561ecbd79e9d8fb2e4d
SyunichiSato <Kurage-dog@outlook.jp>
2017-11-21 15:59:21 +0900

    逃げる町の人
    タイトル通り

---
+++ Assets/Scripts/mobSyoutotu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mobSyoutotu : MonoBehaviour {
+    GameObject human;   // 親オブジェクトの乗客
+                                    // Use this for initialization
+    void Start () {
+        // 親オブジェクトを取得
+        human = transform.root.gameObject;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        human.GetComponent<MobController>().ChangeEscapeRot();
+    }
+}
