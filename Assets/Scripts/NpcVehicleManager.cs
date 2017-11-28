4ce4c22c7d8f9bf75808e59c7dd6b0ad14486fed
elenoa429 <flea0jzs161@gmail.com>
2017-11-28 21:21:47 +0900

    NPCの動きを追加
    NavMeshのBake終わり

---
+++ Assets/Scripts/NpcVehicleManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcVehicleManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void NpcVehicleCreate( GameObject prefab, int tipNum )
+    {
+    }
+}
