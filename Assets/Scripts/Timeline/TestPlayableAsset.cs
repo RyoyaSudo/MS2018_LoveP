0c916e461c2d9851b19c74b61964ecda2d4925cf
RyoyaSudo <sudo.ryoya.hal@gmail.com>
2017-11-30 14:29:15 +0900

    タイムライン・シネマシーン関連導入

---
+++ Assets/Scripts/Timeline/TestPlayableAsset.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+[System.Serializable]
+public class TestPlayableAsset : PlayableAsset
+{
+    public Cinemachine.CinemachineVirtualCamera vcam1;  //Vカメラ
+    public string playerPath;                           //プレイヤーパス
+    public string mainCameraPath;                       //メインカメラパス
+
+    //Timeline再生開始時に１度呼ばれる
+    // Factory method that generates a playable based on this asset
+    public override Playable CreatePlayable(PlayableGraph graph, GameObject go)
+    {
+        // もともとある、CreatePlayableメソッド内で、PlayableBehaviourを以下のようにインスタンス化
+        var behaviour = new TestPlayableBehaviour();
+
+        // Resolveメソッドを利用して、charaObjectの参照を実行時に解決します。
+        behaviour.vcamCamera1 = vcam1;
+        behaviour.playerPath = playerPath;
+        behaviour.mainCameraPath = mainCameraPath;
+
+        return ScriptPlayable<TestPlayableBehaviour>.Create(graph, behaviour);
+    }
+}
