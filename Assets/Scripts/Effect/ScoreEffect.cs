69503af7852dd3d5c1341462915c82e83beb4963
RyoyaSudo <sudo.ryoya.hal@gmail.com>
2017-12-24 18:43:13 +0900

    エフェクト追加準備

---
+++ Assets/Scripts/Effect/ScoreEffect.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreEffect : MonoBehaviour {
+
+    [SerializeField] ParticleSystem particle;
+    public Transform Target { get; set; }
+    bool IsChase { get; set; }
+
+    [SerializeField] float chaseDuration;
+    float chaseAddValuePerFlame;
+    float chaseRate;
+
+    Vector3 getPos;
+
+	// Use this for initialization
+	void Start () {
+        IsChase = false;
+        Target = null;
+        chaseRate = 0.0f;
+        chaseAddValuePerFlame = 0.0f;
+
+        getPos = Vector3.zero;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if( IsChase == true )
+        {
+            chaseRate += chaseAddValuePerFlame;
+            chaseRate = Mathf.Min( chaseRate , 1.0f );
+
+            Vector3 curV = Vector3.Slerp( getPos , Target.position , chaseRate );
+        }
+    }
+}
